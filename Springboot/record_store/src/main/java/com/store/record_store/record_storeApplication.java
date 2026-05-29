5fb6eeaaf4ceb90774e3392b0c8e24fa08e01cbe
JustinDanielaBahamon <dannybahamon613@gmail.com>
2026-05-29 17:19:53 -0500

    Adición de carpetas necesarias para la arquitectura del proyecto

---
+++ Springboot/record_store/src/main/java/com/store/record_store/record_storeApplication.java
@@ -0,0 +1,12 @@
+package com.store.record_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class record_storeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(record_storeApplication.class, args);
+	}
+}
