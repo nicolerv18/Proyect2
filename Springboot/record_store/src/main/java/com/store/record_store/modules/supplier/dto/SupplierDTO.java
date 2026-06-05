8e23ecf5f5d851e82afe855ba00d07f2bc50efd9
Sharik <sharikrojas.sena@gmail.com>
2026-06-05 14:35:08 -0500

    feat: create java class entity Supplier and supplierRepository

---
+++ Springboot/record_store/src/main/java/com/store/record_store/modules/supplier/dto/SupplierDTO.java
@@ -0,0 +1,5 @@
+package com.store.record_store.modules.supplier.dto;
+
+public class SupplierDTO {
+
+}
---
+++ Springboot/record_store/src/main/java/com/store/record_store/modules/supplier/model/Supplier.java
@@ -0,0 +1,5 @@
+package com.store.record_store.modules.supplier.model;
+
+public class Supplier {
+
+}
