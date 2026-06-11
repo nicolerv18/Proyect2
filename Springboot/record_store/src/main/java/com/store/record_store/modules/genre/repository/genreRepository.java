8e8e66ab60959b5418bdfa3edb8ab052de1d3106
JustinDanielaBahamon <dannybahamon613@gmail.com>
2026-06-11 16:31:43 -0500

    fix (genre) se corrige un error en la parte de model y repository

--- Springboot/record_store/src/main/java/com/store/record_store/modules/genre/repository/genreRepository.java
+++ Springboot/record_store/src/main/java/com/store/record_store/modules/genre/repository/genreRepository.java
@@ -4,11 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.store.record_store.modules.genre.model.Genre;
+import com.store.record_store.modules.genre.model.genre;
 import org.springframework.data.jpa.repository.Query;
 
 
-public interface genreRepository extends JpaRepository<Genre, Long> {
+public interface genreRepository extends JpaRepository<genre, Long> {
 
 
     @Query("""
@@ -18,7 +18,7 @@ public interface genreRepository extends JpaRepository<Genre, Long> {
         g.name like %?1%
             """)
     
-    List<Genre> findByName(String name);
+    List<genre> findByName(String name);
     
 }
 
